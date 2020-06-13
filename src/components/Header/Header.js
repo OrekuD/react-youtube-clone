e87b3f47867278950f9a80d49c537cb4b6117d82
unknown <opokudavid446@gmail.com>
2020-06-14 00:01:46 +0100

    Complete header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import { Search } from "../../svg/Svgs";
 
 const Header = ({ value, setValue }) => {
   return (
     <div className={styles.container}>
       <h5 className={styles.logoText}> YouTube </h5>
-      <input className={styles.input} />
+      <div className={styles.searchbar}>
+        <input />
+        <button>
+          <Search color="grey" size="15px" />
+        </button>
+      </div>
     </div>
   );
 };
